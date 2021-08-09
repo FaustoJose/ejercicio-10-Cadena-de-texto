3a39cb1c13f5982406cfecd2a2353cde18d04929
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:44:01 -0400

    Agregar archivos de proyecto.

---
+++ ejercicio 10 Cadena de texto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ejercicio_10_Cadena_de_texto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("La primera letra de esta palabra es :"+Cadena("Hola"));
+        }
+        public static char Cadena(string palabra)
+        {
+            char[] a = palabra.ToCharArray();
+            return a[0];
+        }
+    }
+}
